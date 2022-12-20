20aa3c588fc1e265e9e8d94cb7002898a17240d1
Agrey Tosira <agreytosira@gmail.com>
2022-12-20 23:05:46 +0700

    Fix Everything Messed up

--- js/script.js
+++ js/script.js
@@ -1,11 +1,15 @@
+// Navbar on scroll effect
+
 window.addEventListener("scroll", function () {
     var navbar = document.querySelector(".navbar");
     navbar.classList.toggle("scrolled", window.scrollY > 0);
     if (window.scrollY > 0) {
         navbar.classList.add("navbar-light");
         navbar.classList.remove("navbar-dark");
+        navbar.style.padding = '1rem .5rem';
     } else {
         navbar.classList.add("navbar-dark");
         navbar.classList.remove("navbar-light");
+        navbar.style.padding = '2rem 1rem';
     }
 });
